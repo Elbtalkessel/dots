fd146dcb5eafa4df6b5e0a136d8708bb31f58f9d
Elbtalkessel <netwintercom@gmail.com>
2019-06-11 01:03:18 +0300

    not marry

--- .local/sh/scr.sh
+++ .local/sh/scr.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-scrot -e 'xclip -selection clipboard -t "image/png" < $f'
+scrot -e 'xclip -selection clipboard -t "image/png" < $f && notify-send "Saved: $f"'
 mv *_scrot.png ${HOME}/Pictures/Screenshots 2>&1 > /dev/null
-
