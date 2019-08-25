afc337407d6eb2374875fe0c001dbf178f28057d
Elbtalkessel <netwintercom@gmail.com>
2019-08-25 20:16:41 +0300

    foobar

--- .zsh/zshrc.d/02-profile.zsh
+++ .zsh/zshrc.d/02-profile.zsh
@@ -6,6 +6,7 @@ export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_RUNTIME_DIR="${HOME}/.local/run"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_CONFIG_HOME="${HOME}/.config"
+export MPD_SOCKET="${XDG_RUNTIME_DIR}/mpd.socket"
 
 export GOPATH="${HOME}/.go"
 
