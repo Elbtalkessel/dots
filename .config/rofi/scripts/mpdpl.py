4d31400e5a3b8b3ad8657883afa0d4afe3e55bf9
Elbtalkessel <netwintercom@gmail.com>
2019-09-09 15:15:08 +0200

    navigation between track sources, playlists and mpc controls

--- .config/rofi/scripts/mpdpl.py
+++ .config/rofi/scripts/mpdpl.py
@@ -11,54 +11,75 @@ ROFI = ['rofi', '-theme', 'themes/appsmenu.rasi']
 
 
 def getCurrent():
-	return check_output([*MPC, "current"]);
+    return check_output([*MPC, "current"]);
 
 
 current = getCurrent().decode()
 if not current:
-	current = "Silence"
+    current = "Silence"
 else:
-	current = current.rstrip()
+    current = current.rstrip()
 
 
-titles = check_output([*MPC, "playlist"])
-track = 0
-tlist = ""
-for line in titles.splitlines():
-	track += 1
-	tlist = tlist + "[" + str(track) + "] " + line.decode() + "\n"
-if track > 1:
-	tlist.rstrip()
-	tlist = "[SOURCES]\n[CONTROL]\n" + tlist
-	rofi = Popen([*ROFI, "-selected-row", "1", "-i", "-dmenu", "-p", "Play: "], stdout=PIPE, stdin=PIPE)
-	tmp = rofi.communicate(input=tlist.encode())[0].decode().rstrip()
-	tmp = tmp[tmp.find("[")+1:tmp.find("]")]
-else:
-	tmp = "SOURCES"
-if not tmp:
-	sys.exit(0)
-if tmp == "SOURCES":
-	commands = "Local ~/Music\n"
-	playlists = check_output([*MPC, "lsplaylists"])
-	for line in playlists.splitlines():
-		commands = commands + line.decode() + "\n"
-	rofi = Popen([*ROFI, "-i", "-dmenu", "-p", current], stdout=PIPE, stdin=PIPE)
-	tmp = rofi.communicate(input=commands.encode())[0].decode().rstrip()
-	if not tmp:
-		sys.exit(0)	
-	elif tmp == "Local ~/Music":
-		check_call([*MPC, "clear"])
-		check_call([*MPC, "update"])
-		files = check_output([*MPC, "listall"])
-		add = Popen([*MPC, "add"], stdin=PIPE)
-		add.communicate(files)
-		Popen([*MPC, "play"])
-	else:
-		check_call([*MPC, "clear"])
-		check_call([*MPC, "load", tmp])
-		check_call([*MPC, "play"])
-elif tmp == "CONTROL":
+# list of tracks + SOURCES and CONTROL entries
+def draw_tlist():
+    titles = check_output([*MPC, "playlist"])
+    track = 0
+    tlist = ""
+    tmp = 'SOURCES'
+    for line in titles.splitlines():
+        track += 1
+        tlist = tlist + "[" + str(track) + "] " + line.decode() + "\n"
+    if track > 1:
+        tlist.rstrip()
+        tlist = "[SOURCES]\n[CONTROL]\n" + tlist
+        rofi = Popen([*ROFI, "-selected-row", "1", "-i", "-dmenu", "-p", current], stdout=PIPE, stdin=PIPE)
+        tmp = rofi.communicate(input=tlist.encode())[0].decode().rstrip()
+        tmp = tmp[tmp.find("[") + 1:tmp.find("]")]
+    if not tmp:
+        sys.exit(0)
+    return tmp
+
+
+# list of directories under ~/Music
+def draw_slist():
+    commands = "Local ~/Music\nTRACKS\nCONTROL\n"
+    playlists = check_output([*MPC, "lsplaylists"])
+    for line in playlists.splitlines():
+        commands = commands + line.decode() + "\n"
+    rofi = Popen([*ROFI, "-i", "-dmenu", "-p", current], stdout=PIPE, stdin=PIPE)
+    tmp = rofi.communicate(input=commands.encode())[0].decode().rstrip()
+    if not tmp:
+        sys.exit(0)
+    elif tmp == "Local ~/Music":
+        check_call([*MPC, "clear"])
+        check_call([*MPC, "update"])
+        files = check_output([*MPC, "listall"])
+        add = Popen([*MPC, "add"], stdin=PIPE)
+        add.communicate(files)
+        Popen([*MPC, "play"])
+    if tmp in ['TRACKS', 'CONTROL']:
+        # if None is passed to the main func it will draw list of tracks
+        tmp = tmp if tmp == 'CONTROL' else None
+        main(tmp)
+    else:
+        check_call([*MPC, "clear"])
+        check_call([*MPC, "load", tmp])
+        check_call([*MPC, "play"])
+
+
+def draw_controls():
     check_call([join(ROFI_SH, 'mpdmenu.sh')])
-else:
-	check_call([*MPC, "play", tmp])
 
+
+def main(selected=None):
+    selected = selected or draw_tlist()
+    if selected == 'CONTROL':
+        draw_controls()
+    elif selected == 'SOURCES':
+        draw_slist()
+    else:
+        check_call([*MPC, "play", selected])
+
+
+main()
